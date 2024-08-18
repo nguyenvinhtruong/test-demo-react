848938b6a1d1e404bd540416f0c9a81ab053991e
nguyenvinhtruong <truongvn.1378@gmail.com>
2024-08-18 10:45:35 +0700

    video 21 part 2

--- src/App.js
+++ src/App.js
@@ -5,74 +5,78 @@ import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
 import React from 'react';
 
-class App extends React.Component {
+// class App extends React.Component {
 
-  state = {
-    name: 'Haryphamdev',
-    address: 'Hoi Dan IT',
-    age: 26
-  }
+//   state = {
+//     name: 'Haryphamdev',
+//     address: 'Hoi Dan IT',
+//     age: 26
+//   }
 
-  handleClick = (event) => {
-    console.log('>> Click me my button')
-    console.log('random ', Math.floor((Math.random() * 100) + 1))
+//   handleClick = (event) => {
+//     console.log('>> Click me my button')
+//     console.log('random ', Math.floor((Math.random() * 100) + 1))
 
-    this.setState({
-      name: 'Eric'
-    })
+//     this.setState({
+//       name: 'Eric'
+//     })
 
-  }
+//   }
 
-  handleOnMouseOver(event) {
-    console.log(event.pageX)
-  }
+//   handleOnMouseOver(event) {
+//     console.log(event.pageX)
+//   }
 
-  handleOnChangeInput = (event) => {
-    this.setState({
-      name: event.target.value
-    })
-  }
+//   handleOnChangeInput = (event) => {
+//     this.setState({
+//       name: event.target.value
+//     })
+//   }
 
-  handleOnSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state)
-  }
+//   handleOnSubmit = (event) => {
+//     event.preventDefault();
+//     console.log(this.state)
+//   }
 
-  render() {
-    return (
-      <div>
-        My name is {this.state.name} and I'm from {this.state.address}
-        <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <input
-            type='text'
-            onChange={(event) => this.handleOnChangeInput(event)}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  }
-}
-
-
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello world Slayah
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
+//   render() {
+//     return (
+//       <div>
+//         My name is {this.state.name} and I'm from {this.state.address}
+//         <form onSubmit={(event) => this.handleOnSubmit(event)}>
+//           <input
+//             type='text'
+//             onChange={(event) => this.handleOnChangeInput(event)}
+//           />
+//           <button>Submit</button>
+//         </form>
+//       </div>
+//     );
+//   }
 // }
+
+const App = () => {
+  // const count = useSelector(state => state.counter.count);
+  // const dispatch = useDispatch();
+
+  return (
+
+    <div>
+      Hello world Slayah
+      <MyComponent></MyComponent>
+    </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hello world Slayah
+    //     </p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //   </header>
+    // </div>
+  );
+}
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,58 @@
 import React from "react";
+// class MyComponent extends React.Component {
+//     render() {
+//         return (
+//             <div>my first component</div>
+//         )
+//     }
+// }
+
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Haryphamdev',
+        address: 'Hoi Dan IT',
+        age: 26
+    }
+
+    handleClick = (event) => {
+        console.log('>> Click me my button')
+        console.log('random ', Math.floor((Math.random() * 100) + 1))
+
+        this.setState({
+            name: 'Eric'
+        })
+
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event.pageX)
+    }
+
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+    }
+
     render() {
         return (
-            <div>my first component</div>
-        )
+            <div>
+                My name is {this.state.name} and I'm from {this.state.address}
+                <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                    <input
+                        type='text'
+                        onChange={(event) => this.handleOnChangeInput(event)}
+                    />
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
     }
 }
 
